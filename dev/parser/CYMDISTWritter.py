d58ead5463cb8309667490051043e4c98f7f20f2
Thierry Nouidui <tsnouidui@lbl.gov>
2016-11-14 15:09:08 -0800

    Add libraries so users does not need to set Buildings library path.

--- dev/parser/CYMDISTWritter.py
+++ dev/parser/CYMDISTWritter.py
@@ -40,7 +40,9 @@ CYMDISTModelicaTemplate_MOS = 'CYMDISTModelicaTemplate.mos'
 
 # Get the path to the templates files
 script_path = os.path.dirname(os.path.realpath(__file__))
-utilities_path=os.path.join(script_path, 'utilities')
+utilities_path = os.path.join(script_path, 'utilities', 'templates')
+BUILDINGS_LIB_PATH = os.path.join(script_path, 'utilities', 
+                                'libraries', 'modelica-buildings')
 MO_TEMPLATE_PATH = os.path.join(utilities_path, CYMDISTModelicaTemplate_MO)
 MOS_TEMPLATE_PATH = os.path.join(utilities_path, CYMDISTModelicaTemplate_MOS)
 XSD_FILE_PATH = os.path.join(utilities_path, XSD_SCHEMA)
@@ -67,8 +69,8 @@ def main():
                         help="Path to the Grid model")
     cymdist_group.add_argument('-i', "--input-file-path", required=True,
                         help="Path to the input file")
-    cymdist_group.add_argument("-b", "--buildings-lib-path",
-                        help='Path to the Buildings library, e.g. c:\\test\\xxx\\modelica-buildings')
+#     cymdist_group.add_argument("-b", "--buildings-lib-path",
+#                         help='Path to the Buildings library, e.g. c:\\test\\xxx\\modelica-buildings')
     cymdist_group.add_argument("-r", "--write-results",
                         type=int,
                         help='Flag for writing results. 1 for writing, 0 else. Default is 0.')
@@ -79,15 +81,14 @@ def main():
     # Set defaults for command-line options.
     grid_model_path = args.grid_model_path
     input_file_path = args.input_file_path
-
-    buildings_lib_path = args.buildings_lib_path
-    if (buildings_lib_path is None):
-        log.info('The path to the Buildings library was not provided.')
-        log.info('Start searching the MODELICAPATH to see if it is defined.')
-        buildings_lib_path = os.environ.get('MODELICAPATH')
-        if (buildings_lib_path is None):
-            log.error('The path to the Buildings library was neither'
-                      +' provided nor found on the MODELICAPATH.')
+#     BUILDINGS_LIB_PATH = args.buildings_lib_path
+#     if (BUILDINGS_LIB_PATH is None):
+#         log.info('The path to the Buildings library was not provided.')
+#         log.info('Start searching the MODELICAPATH to see if it is defined.')
+#         BUILDINGS_LIB_PATH = os.environ.get('MODELICAPATH')
+#         if (BUILDINGS_LIB_PATH is None):
+#             log.error('The path to the Buildings library was neither'
+#                       +' provided nor found on the MODELICAPATH.')
     write_results = 0
     
     # Check if any errors
@@ -95,17 +96,17 @@ def main():
         log.error('Missing required input, <path-to-grid-model>')
         parser.print_help()
         sys.exit(1)
-    if(buildings_lib_path is None):
-        log.error('Missing required input, <path-to-buildings-lib>')
-        parser.print_help()
-        sys.exit(1)
+#     if(BUILDINGS_LIB_PATH is None):
+#         log.error('Missing required input, <path-to-buildings-lib>')
+#         parser.print_help()
+#         sys.exit(1)
     if(input_file_path is None):
         log.error('Missing required input, <path-to-input-file>')
         parser.print_help()
         sys.exit(1)
     CYMDIST = CYMDISTWritter(grid_model_path,
                              input_file_path,
-                             buildings_lib_path,
+                             BUILDINGS_LIB_PATH,
                              MO_TEMPLATE_PATH,
                              MOS_TEMPLATE_PATH, 
                              XSD_FILE_PATH,
@@ -150,7 +151,7 @@ def print_cmd_line_usage():
           ' (FMU) for model exchange 2.0')
     print('-- Input -g, Path to the grid model (required).')
     print('-- Input -i, Path to the input file (required).')
-    print('-- Input -b, Path to the Buildings library (required).')
+#     print('-- Input -b, Path to the Buildings library (required).')
     print('-- Option -r, Flag for writing results. 0 if results'+ \
           ' should not be written, 1 else. Default is 0.')
 
@@ -171,7 +172,6 @@ def quit_with_error(messageStr, showCmdLine):
 
     if(showCmdLine):
         print(print_cmd_line_usage())
-
     sys.exit(1)
 
 
@@ -625,8 +625,9 @@ class CYMDISTWritter(object):
         """
 
         # Set the Modelica path to point to the Buildings Library
-        os.environ['MODELICAPATH'] = self.buildings_path
-
+        os.environ['MODELICAPATH'] = self.buildings_path  \
+                                   + ';' + os.environ.get('MODELICAPATH')
+        
         loader = jja2.FileSystemLoader(self.mosT_path)
         env = jja2.Environment(loader=loader)
         template = env.get_template('')
---
+++ dev/parser/utilities/libraries/modelica-buildings/Buildings/package.mo
@@ -0,0 +1,49 @@
+within ;
+package Buildings "Buildings Library with models for building energy and control systems (Subset)"
+  extends Modelica.Icons.Package;
+
+
+
+annotation (
+preferredView="info",
+version="4.0.0",
+versionDate="2016-03-29",
+dateModified="2016-03-29",
+uses(Modelica(version="3.2.2")),
+conversion(
+ from(version={"3.0.0", "4.0.0"},
+      script="modelica://Buildings/Resources/Scripts/Dymola/ConvertBuildings_from_3.0_to_4.0.mos")),
+revisionId="$Id$",
+preferredView="info",
+Documentation(info="<html>
+<p>
+The <code>Buildings</code> library is a free library
+for modeling building energy and control systems.
+Many models are based on models from the package
+<code>Modelica.Fluid</code> and use
+the same ports to ensure compatibility with the Modelica Standard
+Library.
+</p>
+<p>
+The figure below shows a section of the schematic view of the model
+<a href=\"modelica://Buildings.Examples.HydronicHeating\">
+Buildings.Examples.HydronicHeating</a>.
+In the lower part of the figure, there is a dynamic model of a boiler, a pump and a stratified energy storage tank. Based on the temperatures of the storage tank, a finite state machine switches the boiler and its pump on and off.
+The heat distribution is done using a hydronic heating system with a three way valve and a pump with variable revolutions. The upper right hand corner shows a room model that is connected to a radiator whose flow is controlled by a thermostatic valve.
+</p>
+<p align=\"center\">
+<img alt=\"image\" src=\"modelica://Buildings/Resources/Images/UsersGuide/HydronicHeating.png\" border=\"1\"/>
+</p>
+<p>
+The web page for this library is
+<a href=\"http://simulationresearch.lbl.gov/modelica\">http://simulationresearch.lbl.gov/modelica</a>,
+and the development page is
+<a href=\"https://github.com/lbl-srg/modelica-buildings\">https://github.com/lbl-srg/modelica-buildings</a>.
+Contributions to further advance the library are welcomed.
+Contributions may not only be in the form of model development, but also
+through model use, model testing,
+requirements definition or providing feedback regarding the model applicability
+to solve specific problems.
+</p>
+</html>"));
+end Buildings;
