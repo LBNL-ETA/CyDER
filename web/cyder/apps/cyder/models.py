3056250ae8917ac15ecfe9e4fdf8a0c4e7c5c795
Jonathan56 <jonathan.coignard@gmail.com>
2016-12-12 11:26:59 -0800

    Created database structure

--- web/cyder/apps/cyder/models.py
+++ web/cyder/apps/cyder/models.py
@@ -1,6 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Item(models.Model):
-    text = models.TextField(blank=False, null=False)
-    date_posted = models.DateField(auto_now=True)
+class Model(models.Model):
+    """docstring for Model."""
+    filename = models.CharField(max_length=50, null=True, blank=True)
+    north_lat = models.FloatField(null=True, blank=True)
+    south_lat = models.FloatField(null=True, blank=True)
+    north_lon = models.FloatField(null=True, blank=True)
+    south_lon = models.FloatField(null=True, blank=True)
+    upmu_location = models.CharField(max_length=50, null=True, blank=True)
+
+
+class Calibration(models.Model):
+    """docstring for Calibration."""
+    date = models.DateTimeField(null=True, blank=True)
+    model_value = models.FloatField(null=True, blank=True)
+    calibration_value = models.FloatField(null=True, blank=True)
+    updated = models.BooleanField(default=False)
+    calibration_algorithm = models.TextField(null=True, blank=True)
+
+class CalibrationData(models.Model):
+    """docstring for CalibrationData."""
+    p_a = models.FloatField(null=True, blank=True)
+    p_b = models.FloatField(null=True, blank=True)
+    p_c = models.FloatField(null=True, blank=True)
+    q_a = models.FloatField(null=True, blank=True)
+    q_b = models.FloatField(null=True, blank=True)
+    q_c = models.FloatField(null=True, blank=True)
+    voltage_a = models.FloatField(null=True, blank=True)
+    voltage_b = models.FloatField(null=True, blank=True)
+    voltage_c = models.FloatField(null=True, blank=True)
