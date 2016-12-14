e914d146c7c63567e0d75407a53c5f371c0326d7
Jonathan56 <jonathan.coignard@gmail.com>
2016-12-14 14:17:27 -0800

    create a decent home page

--- web/docker_django/apps/cyder/urls.py
+++ web/docker_django/apps/cyder/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^api/home/get/$', views.home_info, name='home_info'),
 ]
--- web/docker_django/apps/cyder/views.py
+++ web/docker_django/apps/cyder/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import Model
 from redis import Redis
 
@@ -7,7 +8,10 @@ redis = Redis(host='redis', port=6379)
 
 
 def home(request):
-    model = Model.objects.all()
-    nb_model = len(model)
-    model_filenames = [m.filename for m in model]
-    return render(request, 'home.html', {'nb_model': nb_model, 'filename': model_filenames})
+    return render(request, 'home.html', {})
+
+def home_info(request):
+    return_dict = {}
+    return_dict['models'] = list(Model.objects.all().values())
+    return_dict['nb_model'] = len(return_dict['models'])
+    return JsonResponse(return_dict)
