d4b738a3bf3833f1d90e1deeb1fdedf7d4e8f412
Jonathan56 <jonathan.coignard@gmail.com>
2017-01-18 14:56:17 -0800

    Wrote the cmd to use upmu server

--- web/docker_django/apps/cyder/upmu.py
+++ web/docker_django/apps/cyder/upmu.py
@@ -7,4 +7,5 @@ import tool as t
 def get(location, datetime):
     date_from = dt.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     date_to = "False"
+    cmd = "ssh cyder@bt-eplus.dhcp.lbl.gov python CyDER/web/docker_django/worker/upmu/get.py 2016-12-15_12:00:00 2016-12-15_12:02:00"
     pass
