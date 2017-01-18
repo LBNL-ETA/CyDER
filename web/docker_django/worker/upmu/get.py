289841dc445e99adb1cc4994b892b3007f429863
Jonathan56 <jonathan.coignard@gmail.com>
2017-01-18 14:35:36 -0800

    final debug

--- web/docker_django/worker/upmu/get.py
+++ web/docker_django/worker/upmu/get.py
@@ -6,6 +6,7 @@ import pytz
 import pandas
 import argparse
 import pdb
+import sys
 
 
 def convert_datetime_to_epoch_ns(dt):
@@ -35,7 +36,7 @@ def get_upmu_data(dates, PMU_name):
     names = ["L1Mag", "L2Mag", "L3Mag", "C1Mag", "C2Mag", "C3Mag",
             "L1Ang", "L2Ang", "L3Ang", "C1Ang", "C2Ang", "C3Ang"]
     result_names = ['P_A', 'Q_A', 'P_B', 'Q_B', 'P_C', 'Q_C', 'VMAG_A',
-                    'VMAG_B', 'VMAG_C', 'datetime', 'units']
+                    'VMAG_B', 'VMAG_C', 'datetime', 'epoch_time', 'units']
 
     # Create the frame holding the measured data and the results
     column_names = []
@@ -50,7 +51,6 @@ def get_upmu_data(dates, PMU_name):
             frame.loc[value_epoch_time, value_name] = query[0][0][1]
 
     # Calculate the results
-    pdb.set_trace()
     frame['P_A'] = (frame['L1Mag']*frame['C1Mag']*np.cos(np.radians(list(frame['L1Ang'] - frame['C1Ang']))))*1e-3
     frame['Q_A'] = (frame['L1Mag']*frame['C1Mag']*np.sin(np.radians(list(frame['L1Ang'] - frame['C1Ang']))))*1e-3
 
@@ -61,6 +61,9 @@ def get_upmu_data(dates, PMU_name):
     frame['Q_C'] = (frame['L3Mag']*frame['C3Mag']*np.sin(np.radians(list(frame['L3Ang'] - frame['C3Ang']))))*1e-3
     frame['units'] = [('kW', 'kVAR', 'V') for index in range(0, len(frame))]
     frame['datetime'] = dates
+
+    # Added for simplicity
+    frame['epoch_time'] = event_times
     frame['VMAG_A'] = frame['L1Mag']
     frame['VMAG_B'] = frame['L2Mag']
     frame['VMAG_C'] = frame['L3Mag']
@@ -68,52 +71,46 @@ def get_upmu_data(dates, PMU_name):
     return frame
 
 
+# Retrieve input
+try:
+    parser = argparse.ArgumentParser(description='Select dates and location')
+    parser.add_argument('date_from')
+    parser.add_argument('date_to')
+    # parser.add_argument('location')
+    args = parser.parse_args()
+    date_from = str(args.date_from)
+    date_to = str(args.date_to)
+    # location = str(args.location)
+except:
+    sys.exit('Error: could not retrieve argument')
+
+# Connect to the database
 connection = btrdb.BTrDBConnection("miranda.cs.berkeley.edu", 4410)
 context = connection.newContext()
 
+# Create datetimes every minutes with "date_from" and "date_to"
 western = pytz.timezone('America/Los_Angeles')
-time_period = western.localize(datetime.datetime(2016, 11, 1, 12, 0, 0))
-time_period2 = western.localize(datetime.datetime(2016, 11, 2, 12, 0, 0))
+date_from = datetime.datetime.strptime(date_from, "%Y-%m-%d_%H:%M:%S")
+if not date_to in "False":
+    date_to = datetime.datetime.strptime(date_to, "%Y-%m-%d_%H:%M:%S")
+    delta_minutes = int((date_to - date_from).total_seconds() / 60)
 
-print("Retrieving data...")
-frame = get_upmu_data([time_period, time_period2], 'grizzly_bus1')
-pdb.set_trace()
+    # Check that the delta minute is reasonable
+    if delta_minutes < 1:
+        raise Exception('Need at least a minute between date_to and date_from')
+    elif delta_minutes > 20000:
+        raise Exception('Limited to 20000 minutes (less than 2 weeks)')
 
+    # Create the list of dates with the appropriate time zone
+    dates = [western.localize(date_from + datetime.timedelta(minutes=x)) for x in range(0, delta_minutes)]
+else:
+    dates = [western.localize(date_from)]
 
-# # Retrieve model name
-# try:
-#     parser = argparse.ArgumentParser(description='Select dates and location')
-#     parser.add_argument('date_from')
-#     parser.add_argument('date_to')
-#     # parser.add_argument('location')
-#     args = parser.parse_args()
-#     date_from = str(args.date_from)
-#     date_to = str(args.date_to)
-#     # location = str(args.location)
-# except:
-#     sys.exit('Error: could not retrieve argument')
-#
-# # Create datetimes every minutes
-# date_from = d.datetime.strptime(date_from, "%Y-%m-%d_%H:%M:%S")
-# if not date_to in "False":
-#     date_to = d.datetime.strptime(date_to, "%Y-%m-%d_%H:%M:%S")
-#     delta_minutes = int((date_to - date_from).total_seconds() / 60)
-#     if delta_minutes < 1:
-#         raise Exception('Needs at least a minute between date_to and date_from')
-#     dates = [date_from + d.timedelta(minutes=x) for x in range(0, delta_minutes)]
-# else:
-#     dates = [date_from]
-#
-# for date in dates:
-#     udata = {'date': date.strftime("%Y-%m-%d_%H:%M:%S"),
-#              'VMAG_A': 7287.4208984375,
-#              'VMAG_B': 7299.921875,
-#              'VMAG_C': 7318.2822265625,
-#              'P_A': 7272.5364248477308,
-#              'P_B': 2118.3817519608633,
-#              'P_C': 6719.1867010705246,
-#              'Q_A': -284.19075651498088,
-#              'Q_B': -7184.1189935099919,
-#              'Q_C': 3564.4269660296022,
-#              'units': ('kW', 'kVAR', 'V')}
-#     print(udata)  # upmu data
+# Get the data from the database for each datetime
+frame = get_upmu_data(dates, 'grizzly_bus1')
+
+# Print the output
+for index in range(0, len(frame)):
+    print(frame.iloc[index][['P_A', 'Q_A', 'P_B', 'Q_B', 'P_C', 'Q_C',
+                             'VMAG_A', 'VMAG_B', 'VMAG_C', 'datetime',
+                             'epoch_time', 'units']].to_dict())
