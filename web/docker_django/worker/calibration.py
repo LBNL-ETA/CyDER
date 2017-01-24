bdbd85b25ef0b592ccb278784f6fb50d6bb00d9a
Jonathan <Jonathan>
2017-01-24 18:27:45 +0000

    fix bug

--- web/docker_django/worker/calibration.py
+++ web/docker_django/worker/calibration.py
@@ -62,8 +62,18 @@ args = parser.parse_args()
 
 # Assign args to variable and cast right format
 udata = {}
-for arg_name in ['P_A', 'P_B', 'P_C', 'Q_A', 'Q_B', 'Q_C', 'VMAG_A', 'VMAG_B', 'VMAG_C']:
-    udate[arg_name] = float(args[arg_name])
+udata['P_A'] = float(args.P_A)
+udata['P_B'] = float(args.P_B)
+udata['P_C'] = float(args.P_C)
+
+udata['Q_A'] = float(args.Q_A)
+udata['Q_B'] = float(args.Q_B)
+udata['Q_C'] = float(args.Q_C)
+
+udata['VMAG_A'] = float(args.VMAG_A)
+udata['VMAG_B'] = float(args.VMAG_B)
+udata['VMAG_C'] = float(args.VMAG_C)
+
 model_filename = str(args.filename)
 breaker_name = str(args.breaker_name)
 breaker_type = str(args.breaker_type)
