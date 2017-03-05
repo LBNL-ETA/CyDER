240478b060b5a3d231d2f7c2ab9459493af8e380
Jonathan56 <jonathan.coignard@gmail.com>
2017-03-05 10:40:43 -0800

    add print('')

--- web/docker_django/worker/simulation/case1.py
+++ web/docker_django/worker/simulation/case1.py
@@ -44,6 +44,7 @@ configuration = func.shift_load_and_pv(load_profile, pv_profile, configuration)
 
 # Create the configuration file
 configuration_filename = func.create_configuration_file(configuration)
+print('')
 print('Configuration file created: ' + configuration_filename.split('//')[-1])
 
 start_time = times[0]
