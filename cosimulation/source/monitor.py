5447098f78657ebe098ebf939d1d230a87e0cb9d
Jonathan56 <jonathan.coignard@gmail.com>
2017-05-11 14:49:34 -0700

    master ready for testing + monitor

--- cosimulation/source/monitor.py
+++ cosimulation/source/monitor.py
@@ -1,1 +1,16 @@
 from __future__ import division
+
+
+class Monitor(object):
+    """Monitor simulation progress"""
+
+    def __init__(self):
+        pass
+
+    def initialize(self, master):
+        """Initialize graphs"""
+        pass
+
+    def visualize(self, master):
+        """Visualize simulation progress"""
+        pass
