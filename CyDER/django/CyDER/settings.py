8043d5e7dfa7cc0afe718f2cb67d1b0456704b6a
MHebant <mhebant@lbl.gov>
2017-09-07 23:30:27 +0200

    Add postgres container and configure Django

--- CyDER/django/CyDER/settings.py
+++ CyDER/django/CyDER/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'CyDER.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'cyder',
+		'USER': 'postgres',
+		'PASSWORD': 'admin',
+		'HOST': 'db',
+		'PORT': '5432',
     }
 }
 
