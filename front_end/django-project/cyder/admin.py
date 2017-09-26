9e5cee195bf7a1951df9363408dd3937f630658b
MHebant <mhebant@lbl.gov>
2017-09-26 16:29:35 -0700

    Add admin interface

--- front_end/django-project/cyder/admin.py
+++ front_end/django-project/cyder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Model)
+admin.site.register(Node)
+admin.site.register(Section)
+admin.site.register(Device)
--- front_end/django-project/cyder/models.py
+++ front_end/django-project/cyder/models.py
@@ -15,6 +15,8 @@ class Model(models.Model):
 
     #def __str__(self):
         #return u"%s %s" % (self.region, self.filename)
+    def __str__(self):
+        return self.filename
 
 class Node(models.Model):
     model = models.ForeignKey(Model, null=True, blank=True)
@@ -23,6 +25,8 @@ class Node(models.Model):
     latitude = models.FloatField(null=True, blank=True)
     class Meta:
         unique_together = ('model', 'node_id',)
+    def __str__(self):
+        return self.node_id
 
 class Section(models.Model):
     model = models.ForeignKey(Model, null=True, blank=True)
@@ -31,6 +35,8 @@ class Section(models.Model):
     to_node = models.ForeignKey(Node, null=True, blank=True,related_name='end_sections')
     class Meta:
         unique_together = ('model', 'section_id',)
+    def __str__(self):
+        return self.section_id
 
 class Device(models.Model):
     model = models.ForeignKey(Model, null=True, blank=True)
@@ -42,6 +48,5 @@ class Device(models.Model):
     longitude = models.FloatField(null=True, blank=True)
     class Meta:
         unique_together = ('model', 'device_number',)
-        
-
-    
+    def __str__(self):
+        return self.device_number
