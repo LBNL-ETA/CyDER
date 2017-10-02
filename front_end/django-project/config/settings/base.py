341eb35ad79c72e65c34f75980123325d18a79c2
MHebant <mhebant@lbl.gov>
2017-10-02 16:57:17 -0700

    Move some security settings from base.py to prod.py

--- front_end/django-project/config/settings/base.py
+++ front_end/django-project/config/settings/base.py
@@ -15,9 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-SESSION_COOKIE_SECURE = True
-CSRF_USE_SESSIONS = True
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
