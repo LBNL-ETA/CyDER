43e499a71fe807e6b4bc597ae20ab3d480c580cf
MHebant <mhebant@lbl.gov>
2017-10-06 16:24:39 -0700

    Add ALLOW_HOSTS setting in dev.py to allow all hosts

--- front_end/django-project/config/settings/dev.py
+++ front_end/django-project/config/settings/dev.py
@@ -4,6 +4,8 @@ SECRET_KEY = '(pb0b1$o6z@iq6)c6parmk!vq3w3)l-mo!#xzzmp!2z-sj^_pn'
 
 DEBUG = True
 
+ALLOWED_HOSTS = ['*']
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
