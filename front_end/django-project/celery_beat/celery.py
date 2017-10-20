2c0b60122cb4cb820303df9256407589add52b16
MHebant <mhebant@lbl.gov>
2017-10-20 16:32:20 -0700

    Set up a celery schedule (celery_beat)

---
+++ front_end/django-project/celery_beat/celery.py
@@ -0,0 +1,27 @@
+from celery import Celery
+from datetime import timedelta
+
+app = Celery('celery_beat',
+             broker='redis://128.3.144.76:6379/0',
+             backend='redis://128.3.144.76:6379/0',
+             include=[
+                'cyder.projects.tasks',
+             ])
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_TRACK_STARTED=True,
+    CELERY_ACCEPT_CONTENT = ['json'],
+    CELERY_RESULT_SERIALIZER = 'json',
+    CELERY_TASK_SERIALIZER = 'json',
+    CELERYBEAT_SCHEDULE = {
+        'retrieve_projects_result': {
+            'task': 'cyder.projects.tasks.retrieve_projects_result',
+            'schedule': timedelta(seconds=1),
+        },
+    },
+)
+
+if __name__ == '__main__':
+    app.start()
---
+++ front_end/django-project/cyder/projects/tasks.py
@@ -0,0 +1,6 @@
+from celery_beat.celery import app
+
+@app.task
+def retrieve_projects_result():
+    
+    return
