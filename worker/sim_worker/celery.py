7592bc7a6891ec5b5b4ab9bac28ff8d3ef261659
MHebant <mhebant@lbl.gov>
2017-10-24 14:40:43 -0700

    Add a queue to sim_worker (called sim_worker)
     To prevent conflict with celery_beat as they share the same broker

--- worker/sim_worker/celery.py
+++ worker/sim_worker/celery.py
@@ -2,6 +2,7 @@
 # If you modify one, please copy/paste the modification in the other one
 
 from celery import Celery
+from kombu import Exchange, Queue
 
 app = Celery('sim_worker',
              broker='redis://128.3.144.76:6379/0',
@@ -12,6 +13,10 @@ app = Celery('sim_worker',
 app.conf.update(
     CELERY_TASK_RESULT_EXPIRES=3600,
     CELERY_TRACK_STARTED=True,
+    CELERY_DEFAULT_QUEUE = 'sim_worker',
+    CELERY_QUEUES = (
+        Queue('sim_worker', Exchange('sim_worker'), routing_key='sim_worker'),
+    )
 )
 
 if __name__ == '__main__':
--- worker/start_worker.bat
+++ worker/start_worker.bat
@@ -1,2 +1,2 @@
-celery -A sim_worker worker -l info -c 1
+celery -A sim_worker worker -l info -c 1 -Q sim_worker
 pause
