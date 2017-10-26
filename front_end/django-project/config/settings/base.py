5a61fee4b9efd60ff19d700a224a88d915607b21
MHebant <mhebant@lbl.gov>
2017-10-26 10:01:18 -0700

    Readd SessionAuthentification to rest API
    Allow to use the web interface for the API

--- front_end/django-project/config/settings/base.py
+++ front_end/django-project/config/settings/base.py
@@ -107,6 +107,6 @@ LOGIN_REDIRECT_URL = '/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
