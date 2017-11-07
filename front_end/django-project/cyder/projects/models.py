1af5dd2096d10be3c361a03699d52af6a3ebd414
MHebant <mhebant@lbl.gov>
2017-11-06 16:19:49 -0800

    Correct bug in Project model wich prevent creation

--- front_end/django-project/cyder/projects/models.py
+++ front_end/django-project/cyder/projects/models.py
@@ -7,12 +7,16 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     task_id = models.CharField(max_length=70, blank=True)
     status = models.CharField(max_length=10, default="NeedSim")
-    settings = models.TextField(blank=True)
-    results = models.TextField(blank=True)
+    settings = models.TextField(default="null")
+    results = models.TextField(default="null")
     def __str__(self):
         return self.name
 
     def __init__(self, *args, **kwargs):
+        if 'settings' in kwargs:
+            kwargs['settings'] = json.dumps(kwargs['settings'])
+        if 'results' in kwargs:
+            kwargs['results'] = json.dumps(kwargs['results'])
         super(Project, self).__init__(*args, **kwargs)
         self.old_settings = self.settings
         self.settings = json.loads(self.settings)
