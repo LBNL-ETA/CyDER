454e99be3930af79ccbaeba8e4ec178e8524ce1f
MHebant <mhebant@lbl.gov>
2017-11-08 18:49:32 -0800

    Split editcreate.js in edit.js and create.js

--- front_end/django-project/cyder/projects/static/cyder/projects/edit.js
+++ front_end/django-project/cyder/projects/static/cyder/projects/edit.js
@@ -1,35 +1,16 @@
-class ProjectEdit extends View {
+class ProjectEditor extends View {
     constructor(el) {
         super(el, 'div');
-        this._childs['select-model'] = new SelectModel(null, false);
-        this.child('select-model').onchange = (e) => {
-            this._addPvMap = new Map();
-            this._loadMapLayers(e.target.value);
-        };
         this._childs['leaflet-map'] = new LeafletMap();
         this._project = null;
         this._addPvMap;
-        this._isNew = null;
-        this.child('select-model').ready.then()
     }
     async loadProject(projectId) {
         this._project = await CyderAPI.Project.get(projectId);
         this._addPvMap = new Map(this._project.settings.addPv.map(obj => [obj.device, obj.power]));
         this._isNew = false;
+        this.render();
         this._loadMapLayers(this._project.settings.model);
-        this.render();
-    }
-    newProject() {
-        this._project = {
-            id: undefined,
-            name: '',
-            settings: {},
-        };
-        this._addPvMap = new Map();
-        this._isNew = true;
-        this.child('select-model').ready.then(() =>
-            this._loadMapLayers(this.child('select-model').modelName));
-        this.render();
     }
     _loadMapLayers(modelName) {
         this.child('leaflet-map').removeLayers();
@@ -44,7 +25,6 @@ class ProjectEdit extends View {
     }
     _writeProject() {
         this._project.name = this._html.name.value;
-        this._project.settings.model = this.child('select-model').modelName;
         this._project.settings.addPv = Array.from(this._addPvMap).map(([device, power]) => ({device, power}));
     }
     async _save(e) {
@@ -63,52 +43,27 @@ class ProjectEdit extends View {
         }
         e.target.classList.remove('disabled');
     }
-    async _create(e) {
+    async _cancel(e) {
         if(e.target.classList.contains('disabled'))
             return;
         e.target.classList.add('disabled');
-        try {
-            this._writeProject();
-            this._project = await CyderAPI.Project.create(this._project);
-            $.notify({message: 'Project created !'},{type: 'success'});
-            this._isNew = false;
-            this.render();
-            history.replaceState(null, null, `./edit/${this._project.id}/`);
-        } catch (error) {
-            if(!(error instanceof CyderAPI.Error))
-                throw(error);
-            error.notify();
-        }
+        await this.loadProject(this._project.id);
         e.target.classList.remove('disabled');
-    }
-    _cancel(e) {
-        this.loadProject(this._project.id);
     }
     render() {
         super.render();
-        if(this._isNew)
-            return;
-        this._html.name.value = this._project.name
-        this.child('select-model').modelName = this._project.settings.model;
+        this._html.name.value = this._project.name;
     }
     get _template() {
         return `
         <div class="form-group">
             <input data-name="name" type="text" class="form-control" placeholder="Name" aria-label="Name">
         </div>
-        ${ IF(this._isNew, () =>
-            `<div class="form-group">
-                <span data-childview="select-model"></span>
-            </div>`
-        )}
+        Model: ${this._project.settings.model}<br>
         <div data-childview="leaflet-map" style="height: 70vh; margin: 1rem 0 1rem 0;"></div>
         <div class="form-group">
-            ${ IF(this._isNew, () =>
-                `<button type="button" data-on="click:_create" class="btn btn-primary">Create</button>`
-            , () =>
-                `<button type="button" data-on="click:_save" class="btn btn-primary">Save</button>
-                <button type="button" data-on="click:_cancel" class="btn btn-primary">Cancel</button>`
-            )}
+            <button type="button" data-on="click:_save" class="btn btn-primary">Save</button>
+            <button type="button" data-on="click:_cancel" class="btn btn-primary">Cancel</button>
         </div>
         `;
     }
