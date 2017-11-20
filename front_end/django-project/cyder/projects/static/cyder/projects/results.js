40c925428dba564a81d9e6dacda29cdb341a1d50
MHebant <mhebant@lbl.gov>
2017-11-20 14:55:11 -0800

    Plot results of projects

--- front_end/django-project/cyder/projects/static/cyder/projects/results.js
+++ front_end/django-project/cyder/projects/static/cyder/projects/results.js
@@ -9,10 +9,44 @@ class ProjectResults extends View {
         this._projectId = projectId;
         let prom = CyderAPI.Project.get(this._projectId, force);
         if(prom instanceof Promise)
-            this._ready = prom.then(() => { this.render() });
+            this._ready = prom.then(() => { this.render(); this._plot(); });
         else
             this._ready = Promise.resolve();
         this.render();
+    }
+    _plot() {
+        let project = CyderAPI.Project.get(this._projectId);
+
+        let times = [];
+
+        let traceHighA = {x: times, y: [], mode: 'lines', name: 'Phase A'};
+        let traceHighB = {x: times, y: [], mode: 'lines', name: 'Phase B'};
+        let traceHighC = {x: times, y: [], mode: 'lines', name: 'Phase C'};
+        let traceLowA = {x: times, y: [], mode: 'lines', name: 'Phase A'};
+        let traceLowB = {x: times, y: [], mode: 'lines', name: 'Phase B'};
+        let traceLowC = {x: times, y: [], mode: 'lines', name: 'Phase C'};
+
+        for(let i = 0; i < project.results.length; i++) {
+            times.push(project.results[i].time);
+            traceHighA.y.push(project.results[i].DwHighVoltWorstA);
+            traceHighB.y.push(project.results[i].DwHighVoltWorstB);
+            traceHighC.y.push(project.results[i].DwHighVoltWorstC);
+            traceLowA.y.push(project.results[i].DwLowVoltWorstA);
+            traceLowB.y.push(project.results[i].DwLowVoltWorstB);
+            traceLowC.y.push(project.results[i].DwLowVoltWorstC);
+        }
+
+        let dataHigh = [traceHighA, traceHighB, traceHighC];
+        let layoutHigh = {
+            title:'DwHighVoltWorst'
+        };
+        Plotly.newPlot(this._html.plotHigh, dataHigh, layoutHigh);
+
+        let dataLow = [traceLowA, traceLowB, traceLowC];
+        let layoutLow = {
+            title:'DwLowVoltWorst'
+        };
+        Plotly.newPlot(this._html.plotLow, dataLow, layoutLow);
     }
     get _template() {
         let project = CyderAPI.Project.get(this._projectId);
@@ -24,22 +58,8 @@ class ProjectResults extends View {
         , () =>
             `<h4>Project: ${escapeHtml(project.name)}</h4>
             <br>
-            <table class="table">
-                <thead>
-                    <tr>
-                      <th scope="col">Time</th>
-                      <th scope="col">DwHighVoltWorstA</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${ FOREACH(project.results, (result) =>
-                        `<tr>
-                            <td>${escapeHtml(result.time)}</td>
-                            <td>${escapeHtml(result.DwHighVoltWorstA)}</td>
-                        </tr>`
-                    )}
-                </tbody>
-            </table>`
+            <div data-name="plotHigh" style="height:300px;"></div>
+            <div data-name="plotLow" style="height:300px;"></div>`
         )}
         `;
     }
