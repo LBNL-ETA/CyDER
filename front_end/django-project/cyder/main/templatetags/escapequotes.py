573d381820fb9e482de4ea46b834722ddceb432e
MHebant <mhebant@lbl.gov>
2017-11-29 14:42:49 -0800

    Replace the custom template filter escapequotes by escapejs from Django

--- front_end/django-project/cyder/main/templatetags/escapequotes.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.template.defaultfilters import stringfilter
-
-register = template.Library()
-
-@register.filter(is_safe=True)
-@stringfilter
-def escapequotes(value):
-    return value.replace('"', '\\"').replace("'", "\\'")
