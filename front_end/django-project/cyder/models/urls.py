fff5ddf8f4189d56d6fea91f609bbf217496f0c9
MHebant <mhebant@lbl.gov>
2017-12-14 16:10:58 -0800

    Change the regex for the model names in urls

--- front_end/django-project/cyder/models/urls.py
+++ front_end/django-project/cyder/models/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.model_viewer, name='model_viewer'),
-    url(r'^(?P<modelname>[0-9a-zA-Z]*)/$', views.model_viewer, name='model_viewer'),
+    url(r'^(?P<modelname>[0-9a-zA-Z _()]*)/$', views.model_viewer, name='model_viewer'),
 ]
