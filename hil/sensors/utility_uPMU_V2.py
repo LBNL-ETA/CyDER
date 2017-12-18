23c325164c95a20e376f00ab1a28bda0be711fd8
ChristophGehbauer <cgehbauer@lbl.gov>
2017-12-18 14:10:30 -0800

    Add files via upload
    Added averaging capability

---
+++ hil/sensors/utility_uPMU_V2.py
@@ -0,0 +1,63 @@
+#This utility script provides support to get the latest value or a range of values from a uPMU database (BTrDB)
+#Documentation of BTrDB: https://github.com/SoftwareDefinedBuildings/btrdb/blob/v3/httpinterface/httpinterface.go
+#Christoph Gehbauer (cgehbauer@lbl.gov) 09/29/2017
+ 
+from requests import get
+from json import loads
+
+def get_latest(uuid, server, fmu=True):
+	#uuid from database
+	#server inclusive port: http://[server]:[port] (standard BTrDB port is 9000)
+	#fmu oprtion: print only value for FMU, or timestep and value if False
+	
+	r = get("{}/q/nearest/{}?time={}&backwards=true".format(server, uuid, 2524608000*10**9))
+	if r.status_code == 200:
+		if fmu:
+			return loads(r.text.encode('ascii','ignore'))[1]
+		else:
+			return loads(r.text.encode('ascii','ignore'))				 
+	else:
+		return -1
+		
+def get_range(uuid, t_start, t_end, server, fmu=True):
+	#uuid from database
+	#t_start as start time of range in nano-seconds (unix time)
+	#t_end as end time of range in nano-seconds (unix time)
+	#server inclusive port: http://[server]:[port] (standard BTrDB port is 9000)
+	#fmu oprtion: print only value for FMU, or timestep and value if False
+	
+	r = get("{}/data/uuid/{}?starttime={}&endtime={}&unitoftime=ns".format(server, uuid, t_start, t_end))
+	if r.status_code == 200:
+		if fmu:
+			return [val for ts, val in loads(r.text.encode('ascii','ignore'))[0]['Readings']]
+		else:
+			return loads(r.text.encode('ascii','ignore'))[0]['Readings']
+	else:
+		return -1
+
+def get_latest_avg(uuid, server, avg, fmu=True):
+	#uuid from database
+	#server inclusive port: http://[server]:[port] (standard BTrDB port is 9000)
+	#fmu oprtion: print only value for FMU, or timestep and value if False
+	
+	r = get("{}/q/nearest/{}?time={}&backwards=true".format(server, uuid, 2524608000*10**9))
+	if r.status_code == 200:
+		if fmu:
+			latest = loads(r.text.encode('ascii','ignore'))[0]
+			range = get_range(uuid, latest-avg*10**9, latest, server, True)
+			return sum(range)/len(range)
+		else:
+			return -1				 
+	else:
+		return -1		
+		
+if __name__ == "__main__":
+	#Example code for LBNL
+	server = "http://upmu.lbl.gov:9000"
+	uuid = "beea4c9c-c5a7-47c3-afb8-8afa956b5553"
+	print 'Test of uPMU queries for {}\nat {}'.format(uuid, server)
+	print 'Latest value as FMU:', get_latest(uuid, server)
+	print 'Latest value as FMU (averge):', get_latest_avg(uuid, server, 1)
+	temp = get_latest(uuid, server, False)
+	print 'Latest value:', temp
+	print 'Latest readings of the last 1 second (120 with uPMU):', len(get_range(uuid, temp[0]-1*10**9, temp[0], server))
