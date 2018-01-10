4735bc6d3751c6e1ae24286d3b21167838a6cbe1
MHebant <mhebant@lbl.gov>
2018-01-10 11:52:11 -0800

    Add component ModelSelector

--- front_end/django-project/cyder/models/static/cyder/models/viewer.js
+++ front_end/django-project/cyder/models/static/cyder/models/viewer.js
@@ -1,1 +1,36 @@
 'use strict';
+import CyderAPI from '../api.js';
+
+export const ModelSelector = {
+    model: { prop: 'value', event: 'change' },
+    props: {
+        allowEmpty: Boolean,
+        value: String,
+        initModels: {
+            default() { return (async () => {
+                return Array.from((await CyderAPI.Model.getAll()).keys());
+            })(); }
+        },
+    },
+    data() { return {
+        models: this.initModels,
+    };},
+    template: `
+        <select @change="$emit('change', $event.target.value)" :value="value" class="custom-select" :disabled="isLoading">
+            <option v-if="allowEmpty" value=""></option>
+            <option v-if="!isLoading" v-for="model in models">{{ model }}</option>
+        </select>`,
+    computed: {
+        isLoading() { return this.models instanceof Promise; },
+    },
+    watch: {
+        models: {
+            immediate: true,
+            handler(value) {
+                if(value instanceof Promise)
+                    value.then((models) => { this.models = models; });
+            },
+        }
+    },
+    updated() { this.$el.value = this.value; }
+};
