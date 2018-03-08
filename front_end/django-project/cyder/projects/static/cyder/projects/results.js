a99c2993b1e2e4fb4973449ca9e6680551515f04
anthonygal <anthony.galtier@gmail.com>
2018-03-07 17:06:18 -0800

    Now displaying some results

--- front_end/django-project/cyder/projects/static/cyder/projects/results.js
+++ front_end/django-project/cyder/projects/static/cyder/projects/results.js
@@ -1,71 +1,140 @@
 'use strict';
-import { View, FOREACH, IF, ESCHTML } from '../viewlib.js';
+import { LeafletMap } from '../models/viewer-legacy.js';
+import { Layer } from '../models/layers.js';
 import CyderAPI from '../api.js';
 import notifyRESTError from '../api-notify-error.js';
 
-export class ProjectResults extends View {
-    constructor(projectId, el) {
-        super(el, 'div');
-        this.loadProject(projectId);
-    }
-    loadProject(projectId, force = false) {
-        this._projectId = projectId;
-        let prom = CyderAPI.Project.get(this._projectId, force);
-        if(prom instanceof Promise) {
-            prom.catch((error) => notifyRESTError(error));
-            this._ready = prom.then(() => { this.render(); this._plot(); });
+
+export const ResultsLayerA = {
+    mixins: [Layer],
+    props: {
+        geojson: null,
+    },
+    data(){
+        return {
         }
-        else {
-            this._ready = Promise.resolve();
+    },
+    methods: {
+        async getLayer() {
+            let pointToLayer = (feature, latlng) => {
+                var circle = L.circle(latlng, {
+                    weight: 2,
+                    fillOpacity: 1,
+                    radius: 3
+                });
+                return circle;
+            }
+            // let onEachFeature = (feature, layer) => {
+            //     feature.setStyle(styleA(feature));
+            // }
+            return L.geoJson(this.geojson, {style: styleA, pointToLayer});
+        },
+    },
+}
+
+export const ResultsLayerB = {
+    mixins: [Layer],
+    props: {
+        geojson: null,
+    },
+    data(){
+        return {
         }
-        this.render();
-    }
-    _plot() {
-        let project = CyderAPI.Project.get(this._projectId);
-
-        let startDate = Date.parse(project.settings.start);
-        let endDate = Date.parse(project.settings.end);
-        let dates = [];
-        for(let date = startDate; date < endDate; date+=project.settings.timestep*1000)
-            dates.push(new Date(date));
-
-        let traceHighA = {x: dates, y: project.results.DwHighVoltWorstA, mode: 'lines', name: 'Phase A'};
-        let traceHighB = {x: dates, y: project.results.DwHighVoltWorstB, mode: 'lines', name: 'Phase B'};
-        let traceHighC = {x: dates, y: project.results.DwHighVoltWorstC, mode: 'lines', name: 'Phase C'};
-        let dataHigh = [traceHighA, traceHighB, traceHighC];
-        let layoutHigh = {
-            title:'Downstream worst high voltage',
-            yaxis: {
-                title: 'Percentage(%)'
+    },
+    methods: {
+        async getLayer() {
+            let pointToLayer = (feature, latlng) => {
+                var circle = L.circle(latlng, {
+                    weight: 2,
+                    fillOpacity: 1,
+                    radius: 3
+                });
+                return circle;
             }
-        };
-        Plotly.newPlot(this._html.plotHigh, dataHigh, layoutHigh);
+            // let onEachFeature = (feature, layer) => {
+            //     feature.setStyle(styleA(feature));
+            // }
+            return L.geoJson(this.geojson, {style: styleB, pointToLayer});
+        },
+    },
+}
 
-        let traceLowA = {x: dates, y: project.results.DwLowVoltWorstA, mode: 'lines', name: 'Phase A'};
-        let traceLowB = {x: dates, y: project.results.DwLowVoltWorstB, mode: 'lines', name: 'Phase B'};
-        let traceLowC = {x: dates, y: project.results.DwLowVoltWorstC, mode: 'lines', name: 'Phase C'};
-        let dataLow = [traceLowA, traceLowB, traceLowC];
-        let layoutLow = {
-            title:'Downstream worst low voltage',
-            yaxis: {
-                title: 'Percentage(%)'
+export const ResultsLayerC = {
+    mixins: [Layer],
+    props: {
+        geojson: null,
+    },
+    data(){
+        return {
+        }
+    },
+    methods: {
+        async getLayer() {
+            let pointToLayer = (feature, latlng) => {
+                var circle = L.circle(latlng, {
+                    weight: 2,
+                    fillOpacity: 1,
+                    radius: 3
+                });
+                return circle;
             }
-        };
-        Plotly.newPlot(this._html.plotLow, dataLow, layoutLow);
+            // let onEachFeature = (feature, layer) => {
+            //     feature.setStyle(styleA(feature));
+            // }
+            return L.geoJson(this.geojson, {style: styleC, pointToLayer});
+        },
+    },
+}
+
+function getColor(v) {
+    let color;
+    switch(v!=null) {
+        case (v<=0.95):
+            color='#130084';
+            break;
+        case (v<=0.96):
+            color='#1933a4';
+            break;
+        case (v<=0.97):
+            color='#1f88c9';
+            break;
+        case (v<=0.98):
+            color='#4efeb6';
+            break;
+        case (v<=0.99):
+            color='#20eba0';
+            break;
+        case (v<=1.01):
+            color='#32cd32';
+            break;
+        case (v<=1.02):
+            color='#bbff0f';
+            break;
+        case (v<=1.03):
+            color='#fad428';
+            break;
+        case (v<=1.04):
+            color='#f99e28';
+            break;
+        case (v<=1.05):
+            color='#f9600b';
+            break;
+        case (v>1.05):
+            color='#e80101';
+            break;
     }
-    get _template() {
-        let project = CyderAPI.Project.get(this._projectId);
-        return `
-        <h1>Results</h1>
-        ${ IF(project instanceof Promise, () =>
-            `<br>
-            Loading...`
-        , () =>
-            `<h4>Project: ${ESCHTML(project.name)}</h4>
-            <br>
-            <div data-name="plotHigh" style="height:300px;"></div>
-            <div data-name="plotLow" style="height:300px;"></div>`
-        )}
-        `;
-    }
+    return color;
+}
+
+function styleA (feature) {
+    if (feature.properties.vA!=null) {return { color: getColor(feature.properties.vA)};}
+    else {return { color: '#808080', opacity: 0.5};}
+}
+function styleB (feature) {
+    if (feature.properties.vB!=null) {return { color: getColor(feature.properties.vB)};}
+    else {return { color: '#808080', opacity: 0.5};}
+}
+function styleC (feature) {
+    if (feature.properties.vC!=null) {return { color: getColor(feature.properties.vC)};}
+    else {return { color: '#808080', opacity: 0.5};}
 }
